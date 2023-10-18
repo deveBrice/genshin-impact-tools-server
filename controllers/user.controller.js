2cece81f2bc5a5ab89e3e4150376921d204a9384
deveBrice <brice.william971@gmail.com>
2023-10-18 16:25:38 +0200

    connection to genshin-impact-tools

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.model');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 exports.userId = (userId) => {
@@ -21,6 +22,35 @@ exports.signup = (req, res, next) => {
       user.save()
         .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
         .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.signin = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if (!user) {
+            return res.status(401).json({ message: 'login ou mot de passe incorrecte'});
+        }
+        bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+                if (!valid) {
+                    return res.status(401).json({ message: 'login ou mot de passe incorrecte' });
+                }
+                
+                res.status(200).json({
+                    userId: user._id,
+                    pseudo: user.pseudo,
+                    token: jwt.sign(
+                        { userId: user._id },
+                        'RANDOM_TOKEN_SECRET',
+                        { expiresIn: '24h' }
+                    )
+                });
+
+               this.getUserId(user._id)
+            })
+            .catch(error => res.status(500).json({ error }));
     })
     .catch(error => res.status(500).json({ error }));
 };
